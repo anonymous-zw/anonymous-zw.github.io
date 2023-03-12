95fce8fc420ebe902681e0da4a6d2c5b7f2535d5
SimpleBinary <simplebinary@yahoo.com>
2023-03-12 13:04:51 +0200

    Initial Draft

---
+++ src/lib/cover/Cover.svelte
@@ -0,0 +1,118 @@
+<script>
+    import anime from 'animejs/lib/anime.es.js';
+    import image from "./image.jpg";
+    import ArrowDown from "svelte-material-icons/ArrowDown.svelte";
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        anime.timeline({loop: false}).add({
+            targets: '.heading .letter',
+            translateY: [-100,0],
+            easing: "easeOutExpo",
+            duration: 1400,
+            delay: (el, i) => 35 * i
+        })
+    });
+</script>
+
+<div class="section">
+    <div class="wrapper">
+        <div class="cover" style="background-image: url({image})">
+            <div class="tint">
+                
+            </div>
+            <h1 class="heading">{@html "Anonymous Clothing".replace(/\S/g, "<span class='letter'>$&</span>")}</h1>
+            <div class="arrow">
+                <ArrowDown color="#ffffff" size="36px" />
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .arrow {
+        position: fixed;
+        bottom: 80px;
+        opacity: 0;
+        animation: arrow 1s ease-in forwards;
+    }
+
+    .heading {
+        font-size: 2rem;
+        font-weight: 500;
+        color: #ffffff;
+        text-transform: uppercase;
+        letter-spacing: 0.4em;
+        overflow: hidden;
+        padding: 40px 0;
+    }
+
+    .section, .wrapper, .cover {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .section, .wrapper {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        width: 100vw;
+        height: 100vh;
+    }
+
+    .cover {
+        width: 100%;
+        height: 100%;
+        filter: grayscale(0.6);
+        background-size: cover;
+        background-position: center;
+        border-radius: 0;
+        animation: reveal 1s ease-out forwards;
+    }
+
+    .tint {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background-color: #33313400;
+        animation: fade 1s ease-out infinite;
+        animation-play-state: paused;
+        animation-delay: calc(var(--scroll) * -2s);
+        animation-iteration-count: 1;
+        animation-fill-mode: both;
+    }
+
+    .wrapper {
+        overflow: hidden;
+        animation: grow 1s ease-out forwards;
+    }
+
+    @keyframes grow {
+        to {
+            width: calc(100vw - 4em);
+            height: calc(100vh - 4em);
+            border-radius: 1.5em;
+        }
+    }
+
+    @keyframes reveal {
+        to {
+            transform: scale(1.1);
+            filter: grayscale(0);
+        }
+    }
+
+    @keyframes fade {
+        to {
+            background-color: #333134ff;
+            backdrop-filter: blur(100px);
+        }
+    }
+
+    @keyframes arrow {
+        to {
+            opacity: 1;
+        }
+    }
+</style>
---
+++ src/lib/section/Section.svelte
@@ -0,0 +1,63 @@
+<script>
+	export let style;
+	export let image;
+	export let title;
+	export let text;
+	export let page;
+</script>
+
+<div class="section style-{style}">
+	<div class="wrapper">
+		<img class="section-image" src={image} alt={title}>
+	</div>
+	<div class="info">
+		<h1>{title}</h1>
+		<p>{text}</p>
+		<a href={page}>See more...</a>
+	</div>
+</div>
+
+<style>
+	h1 {
+		font-family: "Playfair Display";
+		color: #3f3d34;
+	}
+
+	.section, .style-0 {
+		height: 60vh;
+		margin: 0 2em;
+		margin-bottom: 2em;
+		display: grid;
+		grid-template-columns: 50% auto;
+		grid-template-rows: 100%;
+		gap: 2em;
+	}
+
+	.style-1 {
+		grid-template-columns: auto 50%;
+	}
+	
+	.style-1 img {
+		order: 1;
+	}
+
+	.style-1 .info {
+		order: -1;
+	}
+
+	img, .wrapper {
+		height: 100%;
+		width: 100%;
+		border-radius: 2em;
+	}
+	
+	img {
+		object-fit: cover;
+		object-position: center;
+		filter: grayscale(0.6);
+	}
+
+	.wrapper {
+		overflow: hidden;
+	}
+</style>
